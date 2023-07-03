ab0cdcae989d8141888ffdd12ca01e3b869ad7cb
WellZup <wellington.oliveira@zup.com.br>
2023-07-03 10:59:35 -0300

    :green_heart:Concertando bug na linha 31

--- src/ConversorDeMoedas/ConversorDeMoedas.java
+++ src/ConversorDeMoedas/ConversorDeMoedas.java
@@ -1,15 +1,15 @@
 package ConversorDeMoedas;
 
 import java.math.BigDecimal;
+import java.util.Scanner;
 
-public class ConversorDeMoedas {
+class ConversorDeMoeda {
     private BigDecimal taxaDolarAmericano;
     private BigDecimal taxaDolarCanadense;
     private BigDecimal taxaEuro;
     private BigDecimal taxaLibraEsterlina;
 
-    public ConversorDeMoedas(BigDecimal taxaDolarAmericano, BigDecimal taxaDolarCanadense,
-                             BigDecimal taxaEuro, BigDecimal taxaLibraEsterlina) {
+    public ConversorDeMoeda(BigDecimal taxaDolarAmericano, BigDecimal taxaDolarCanadense, BigDecimal taxaEuro, BigDecimal taxaLibraEsterlina) {
         this.taxaDolarAmericano = taxaDolarAmericano;
         this.taxaDolarCanadense = taxaDolarCanadense;
         this.taxaEuro = taxaEuro;
@@ -17,16 +17,15 @@ public class ConversorDeMoedas {
     }
 
     public BigDecimal converterParaReal(String moeda, BigDecimal valor) {
-        switch (moeda){
-            case  "Dolar Americano" :
+        switch (moeda) {
+            case "Dólar americano":
                 return valor.multiply(taxaDolarAmericano);
-            case "Dolar Canadense" :
+            case "Dólar canadense":
                 return valor.multiply(taxaDolarCanadense);
-            case "Euro" :
+            case "Euro":
                 return valor.multiply(taxaEuro);
-            case "Libra Esterlina" :
+            case "Libra Esterlina":
                 return valor.multiply(taxaLibraEsterlina);
-
             default:
                 throw new IllegalArgumentException("Tipo de moeda inválido");
         }
--- src/ConversorDeMoedas/Main.java
+++ src/ConversorDeMoedas/Main.java
@@ -5,57 +5,42 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    Scanner entrada = new Scanner(System.in);
-    ConversorDeMoedas conversor = new ConversorDeMoedas(new BigDecimal("5.43"),new BigDecimal("4.21"),
-            new BigDecimal("6.22"),new BigDecimal("7.34"));
+        Scanner scanner = new Scanner(System.in);
+        ConversorDeMoeda conversor = new ConversorDeMoeda(new BigDecimal("5.43"), new BigDecimal("4.21"), new BigDecimal("6.22"), new BigDecimal("7.34"));
 
-    while (true) {
+        while (true) {
+            System.out.println("Boas vindas ao conversor de moedas");
+            System.out.println("Qual a moeda que você quer converter?");
+            System.out.println("1. Dólar americano");
+            System.out.println("2. Dólar canadense");
+            System.out.println("3. Euro");
+            System.out.println("4. Libra Esterlina");
+            String moeda = scanner.nextLine();
 
+            try {
+                System.out.println("O programa só sabe converter dinheiro para Real (R$)");
+                System.out.println("Digite o valor a ser convertido:");
+                String valorStr = scanner.nextLine();
+                BigDecimal valor = new BigDecimal(valorStr);
 
-        System.out.println("Bem vindo ao conversor de moedas.");
-        System.out.println("Escolha no menu, qual moeda a ser convertida: ");
+                if (valor.compareTo(BigDecimal.ZERO) <= 0) {
+                    System.out.println("Valor inválido, por favor, tente novamente");
+                    continue;
+                }
 
-        System.out.println("===== Menu =====");
-        System.out.println("1. Dólar americano");
-        System.out.println("2. Dólar canadense");
-        System.out.println("3. Euro");
-        System.out.println("4. Libra Esterlina");
-        System.out.println("5. Encerrar o processo");
-        System.out.println("Escolha uma opção: ");
-
-
-        try {
-            System.out.println("O programa só sabe converter dinheiro para Real (R$)");
-            System.out.println("Digite o valor a ser convertido:");
-
-            String valor = entrada.nextLine();
-            BigDecimal valorConver = new BigDecimal(valor);
-
-            if (valor.compareTo(String.valueOf(BigDecimal.ZERO)) <= 0) {
-                System.out.println("Valor inválido, por favor, tente novamente");
-                continue;
+                BigDecimal valorConvertido = conversor.converterParaReal(moeda, valor);
+                System.out.printf("O valor que pediu para converter de %s em reais é R$%.2f%n", valor, valorConvertido);
+            } catch (NumberFormatException erro) {
+                System.out.println("Para moeda, o valor monetário deve ser um número decimal");
+            } catch (IllegalArgumentException erro) {
+                System.out.println(erro.getMessage());
             }
 
-            BigDecimal coversor = conversor.converterParaReal(moeda,valor);
-
-            System.out.printf("O valor que pediu para converter de %s em reais é R$%.2f%n", valor, valorConver);
-        } catch (NumberFormatException e) {
-            System.out.println("Para moeda, o valor monetário deve ser um número decimal");
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
+            System.out.println("Deseja fazer outra conversão? (s/n)");
+            String resposta = scanner.nextLine();
+            if (!resposta.equalsIgnoreCase("s")) {
+                break;
+            }
         }
-
-        System.out.println("Deseja fazer outra conversão? (s/n)");
-        String resposta = entrada.nextLine();
-        if (!resposta.equalsIgnoreCase("s")) {
-            break;
-        }
-
-        }
-        }
-
+    }
 }
-
-
-
-
